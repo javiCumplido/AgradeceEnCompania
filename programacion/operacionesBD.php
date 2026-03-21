f3661562b10ee5512d3c00893fa6683afc98689a
Javier Cumplido Barahona <javiercumplidobarahona.guadalupe@alumnado.fundacionloyola.net>
2026-03-21 13:26:50 +0100

    Proyecto agradece en compañia | INICIO PROYECTO

---
+++ programacion/operacionesBD.php
@@ -0,0 +1,47 @@
+<?php
+
+include 'constantes.php';
+
+
+
+function conectar()
+{
+
+  $conexion = new mysqli(SERVER, USER, PASSWORD, BD1);
+
+  $conexion->set_charset("utf8");
+
+  return $conexion;
+
+}
+
+
+
+function recogerUsuarios()
+{
+
+  $conexion = conectar();
+
+  $sql = 'SELECT idAlumno, nombre FROM Usuario';
+
+  $resultado = $conexion->query($sql);
+
+  for ($i = 0; $fila = $resultado->fetch_array() and $i < 2; $i++)
+  {
+
+    echo '<option value="' . $fila["idAlumno"] . '"' . '>' . $fila["nombre"] . '</option>';
+  }
+
+  $conexion->close();
+
+}
+
+
+
+function saludar()
+{
+
+  echo "Hola";
+
+}
+
